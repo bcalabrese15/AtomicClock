f9f035c13c4b202f0a5b4f04da44ea561746bb29
Ben <50995624+bcalabrese15@users.noreply.github.com>
2019-07-08 15:13:36 -0400

    I uploaded the wrong files
    :(

--- ClockInitialize.java
+++ ClockInitialize.java
@@ -1,6 +1,10 @@
 package AtomicClockPackage;
 
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ClockInitialize
 //Initialized the clock by gathering data from the publicly available NIST server
@@ -11,6 +15,18 @@ public class ClockInitialize
 	public static void serverCall()
 	{
 		//Get data from server
+		try {
+			Socket serverData = new Socket(SERVER, PORT);
+			BufferedReader byteData = new BufferedReader
+					(new InputStreamReader(serverData.getInputStream()));
+		} catch (UnknownHostException e) {
+			System.out.println("UnknownHostException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		}
+		
 		//Do I need a return statement? Maybe
 	}
 	public String toString()
--- main.java
+++ main.java
@@ -3,4 +3,9 @@ package AtomicClockPackage;
 public class main 
 {
 	//Test
+	public static void main()
+	{
+		ClockInitialize clock = new ClockInitialize();
+		System.out.println(clock.toString());
+	}
 }
