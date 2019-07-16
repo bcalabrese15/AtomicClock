3872be59b852a765c4d976deb15e537d6dd9b82a
Ben <50995624+bcalabrese15@users.noreply.github.com>
2019-07-16 11:20:58 -0400

    Finished
    Officially done.
    A few notes:
    -I think I understand what BufferedReader does now
    -For some reason a NullPointerException gets thrown while reading, but I believe it's the fault of the server
    -Converted from military time to AM/PM, kept the Universal timezone instead of converting to EST
    Got quite a bit of help from this: https://www.rgagnon.com/javadetails/java-0589.html
    
    Next project will probably be AI focused, since this was just to get back into the programming groove

--- ClockInitialize.java
+++ ClockInitialize.java
@@ -1,6 +1,8 @@
 package AtomicClockPackage;
 
 import java.net.Socket;
+
+import java.util.Arrays;
 import java.net.UnknownHostException;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,17 +14,17 @@ public class ClockInitialize
 {
 	public static final String SERVER = "129.6.15.29";//NIST, Gaithersburg, Maryland, time-b-g.nist.gov 
 	public static final int PORT = 13;
-	public static String temp;
+	public static String timeString;
 	public static void serverCall()
 	{
 		//Get data from server
 		try {
 			Socket serverData = new Socket(SERVER, PORT); //connects to the server and port
 			BufferedReader byteData = new BufferedReader
-					(new InputStreamReader(serverData.getInputStream())); //created bufferedreader(?) from the server
+					(new InputStreamReader(serverData.getInputStream())); //creates a buffered reader which reads the output from the server
 			 while (true) {
-		          if ( (temp = byteData.readLine()).indexOf("*") > -1) {
-		             break; //if the bufferedreader encounters a "*", the reader stops(?)
+		          if ((timeString = byteData.readLine()).indexOf("*") > -1) {//sometimes this line doesn't work for some reason. I believe it's a server-side issue
+		             break; //if the bufferedreader encounters a "*", the reader stops reading the input
 		          }
 		       }
 		} catch (UnknownHostException e) {
@@ -31,13 +33,29 @@ public class ClockInitialize
 		} catch (IOException e) {
 			System.out.println("IOException");
 			e.printStackTrace();
-		}
+		}//try/catch for possible exceptions from the server
 	}
 	public String toString()
 	{
 		//Returns the time in String form.
 		ClockInitialize c = new ClockInitialize();
 		c.serverCall(); //initialize and call the server
-		return temp;//i'll need to replace this
+		char[] timeArray = Arrays.copyOfRange(timeString.toCharArray(), 15, 23);
+		if(timeArray[0] == '1' && (int) timeArray[1] > 2)//converting from military to AM/PM
+		{
+			timeArray[0] = '0';
+			int temp = (int) timeArray[1];
+			temp = temp - 2;//this seems messy but I don't know how to do it another way
+			timeArray[1] = (char) temp;
+			String time = new String(timeArray);
+			return time + " PM UTC"; 
+		}
+		String time = new String(timeArray);
+		return time + " AM UTC";
+	}
+	public static void main(String args[])
+	{
+		ClockInitialize clock = new ClockInitialize();//initializes clock object
+		System.out.println(clock.toString());//prints
 	}
 }
